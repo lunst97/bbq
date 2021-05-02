32e2306cc5b3d2b51092465c89811f602d2570dc
LunST <lunststunt@gmail.com>
2021-05-03 01:34:31 +0300

    fix alert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level.to_sym
+    when :notice then "alert-info"
+    when :success then "alert-success"
+    when :error then "alert-danger"
+    when :alert then "alert-warning"
+    else "alert-#{flash_type}"
+    end
+  end
   def user_avatar(user)
     # TODO: user real avatar
     asset_pack_path('media/images/user.png')
