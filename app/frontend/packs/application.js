18064846f509360d235439ebc70f79880cd3a731
LunST <lunststunt@gmail.com>
2021-05-03 17:51:30 +0300

    fix texture / add calendar / fix grid in event

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,5 +1,18 @@
 import Rails from "@rails/ujs"
 import 'bootstrap/dist/js/bootstrap'
 import '../styles/application.scss'
+import '../scripts/datepicker'
+import '../scripts/datepicker.min'
+import '../scripts/i18n/datepicker.cs'
 const images = require.context('../images', true)
 Rails.start()
+
+$(document).ready(function (){
+    $('#event_datetime').datepicker({
+        timepicker: true,
+        altField: "dd.mm.yyyy hh:mm",
+        timeFormat: "hh:ii",
+        todayButton: new Date(),
+        minDate: new Date()
+    })
+})
