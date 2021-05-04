614df45b291539e30397c7a0a8284ba15151c0be
LunST <lunststunt@gmail.com>
2021-05-04 16:56:13 +0300

    fix validation

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,16 +2,13 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  # Обязательно должно быть событие
-  validates :event, presence: true
-
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
   validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
   validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
 
-  validate :current_user_not_subscription, if: -> { user.present? }
+  validate :check_for_self_subscription, if: -> { user.present? }
 
   # Если есть юзер, выдаем его имя,
   # если нет – дергаем исходный метод
@@ -33,8 +30,7 @@ class Subscription < ApplicationRecord
     end
   end
 
-  def current_user_not_subscription
-    errors.add(:user, message: I18n.t('activerecord.attributes.subscriptions.errors')) if event.user == user
+  def check_for_self_subscription
+    errors.add(:base, message: I18n.t('activerecord.attributes.subscriptions.errors')) if event.user == user
   end
-
 end
