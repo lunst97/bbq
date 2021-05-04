d6dd0bfc0dd58840274522a2060e7e73c09d26aa
LunST <lunststunt@gmail.com>
2021-05-04 13:45:24 +0300

    setting comments and subscriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,8 @@ module ApplicationHelper
     # TODO: user real avatar
     asset_pack_path('media/images/user.png')
   end
+
+  def fa_icon(icon_class)
+    content_tag(:span, '', class: "fa fa-#{icon_class}")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  resources :subscriptions
+  resources :comments
+
   devise_for :users
+
   resources :users, only: [:show, :edit, :update]
-  resources :events
+
+  resources :events do
+    resources :comments, only: [:create, :destroy]
+    resources :subscriptions, only: [:create, :destroy]
+  end
+
   root "events#index"
 end
