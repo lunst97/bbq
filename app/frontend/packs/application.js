2203edb56b77f3ac2d5f0d67621e58b4e436fa18
LunST <lunststunt@gmail.com>
2021-05-05 22:56:55 +0300

    fix datepicker

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,7 +2,7 @@ import Rails from "@rails/ujs"
 import 'bootstrap/dist/js/bootstrap'
 import 'styles/application.scss'
 import 'air-datepicker/dist/js/datepicker.min'
-import 'air-datepicker/dist/js/i18n/datepicker.ru'
+import 'air-datepicker/dist/js/i18n/datepicker.cs'
 import '../scripts/datetime'
 
 const images = require.context('../images', true)
