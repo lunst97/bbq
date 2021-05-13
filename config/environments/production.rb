61f5f75336208e80cee45d49eaff53cfd7916d15
LunST <lunststunt@gmail.com>
2021-05-14 01:23:41 +0300

    fix mailjet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  
+
   ActionMailer::Base.smtp_settings = {
     address: 'in-v3.mailjet.com',
     port: '587',
