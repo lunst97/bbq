4689cae653fe24736039072d3456f536afb11abc
LunST <lunststunt@gmail.com>
2021-05-14 02:33:09 +0300

    fix mailjet

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,6 +1,6 @@
 Mailjet.configure do |config|
     config.api_key = ENV['MAILJET_API_KEY']
     config.secret_key = ENV['MAILJET_SECRET_KEY']
-    config.default_from = ENV['MAILJET_PROD_EMAIL']
+    config.default_from = ENV['MAILJET_DEFAULT_EMAIL']
     config.api_version = 'v3.1'
 end
