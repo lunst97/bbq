971a9cc50653a6d55ceeb64ee3175cfa380c343c
LunST <lunststunt@gmail.com>
2021-05-14 02:22:08 +0300

    fix mailjet

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,15 +1,6 @@
-if Rails.env.development?
-  Mailjet.configure do |config|
-    config.api_key = ENV['MAILJET_API_KEY']
-    config.secret_key = ENV['MAILJET_SECRET_KEY']
-    config.default_from = ENV['MAILJET_DEV_EMAIL']
-    config.api_version = 'v3.1'
-  end
-elsif Rails.env.production?
-  Mailjet.configure do |config|
+Mailjet.configure do |config|
     config.api_key = ENV['MAILJET_API_KEY']
     config.secret_key = ENV['MAILJET_SECRET_KEY']
     config.default_from = ENV['MAILJET_PROD_EMAIL']
     config.api_version = 'v3.1'
-  end
 end
