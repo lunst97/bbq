64d0d609d02de0368bb86bf99a5236ae3f5cc3b5
LunST <lunststunt@gmail.com>
2021-06-15 01:07:44 +0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :deploy_to, 'home/deploy/www'
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
