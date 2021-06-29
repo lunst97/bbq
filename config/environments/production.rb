428e8272b99ecad7859ca3146a1d764ff72ae663
LunST <lunststunt@gmail.com>
2021-06-29 14:01:34 +0300

    refactor vk outh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,7 +106,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'http://bbq-events.ru' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :mailjet
   config.action_mailer.smtp_settings = {
     address: 'in-v3.mailjet.com',
     port: '587',
