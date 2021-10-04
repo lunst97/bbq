0581f2beb8e5ec95a20be539c63a60653f74436c
LunST <lunststunt@gmail.com>
2021-10-04 18:48:10 +0300

    Added sidekiq in heroku and settings S3

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   resources :subscriptions
   resources :comments
 
-
-  mount Sidekiq::Web => '/sidekiq'
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+    end
+    mount Sidekiq::Web, at: '/sidekiq'
 
   # mount Resque::Server.new, :at => "/resque"
 
